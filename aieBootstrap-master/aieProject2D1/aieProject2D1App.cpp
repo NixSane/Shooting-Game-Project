3bd1a37967ba165ac561288e01a768c7ac5893a7
Nixon Sok <nixon.s.aie@outlook.com>
2019-04-29 14:25:19 +1000

    The Game Project

---
+++ aieBootstrap-master/aieProject2D1/aieProject2D1App.cpp
@@ -0,0 +1,142 @@
+#include "aieProject2D1App.h"
+#include "Texture.h"
+#include "Font.h"
+#include "Input.h"
+
+aieProject2D1App::aieProject2D1App() {
+
+}
+
+aieProject2D1App::~aieProject2D1App() {
+
+}
+
+bool aieProject2D1App::startup() {
+	
+	m_2dRenderer = new aie::Renderer2D();
+
+	// TODO: remember to change this when redistributing a build!
+	// the following path would be used instead: "./font/consolas.ttf"
+	m_font = new aie::Font("../bin/font/consolas.ttf", 32);
+
+	m_faceTexture = new aie::Texture("./textures/car2.png");
+	m_background = new aie::Texture("./textures/background.png");
+	m_sunny_road = new aie::Texture("./textures/background_sunny.png");
+	m_world_track = new aie::Texture("./textures/bg_world_track.png");
+	m_ship = new aie::Texture("./textures/ship_2 - Copy.png");
+
+	m_timer = 0;
+
+	return true;
+}
+
+void aieProject2D1App::shutdown() {
+
+	delete m_font;
+	delete m_2dRenderer;
+	delete m_faceTexture;
+	delete m_ship;
+}
+
+void aieProject2D1App::update(float deltaTime) {
+
+	/*m_timer += deltaTime;*/
+
+	// input example
+	aie::Input* input = aie::Input::getInstance();
+	
+
+	// Car rotation
+	float s = sinf(rotation) * speed;
+	float c = cosf(rotation) * speed;
+	x -= s * deltaTime;
+	y += c * deltaTime;
+
+
+	//// Set camera to player
+	//float cam_posX;
+	//float cam_posY;
+	//m_2dRenderer->getCameraPos(cam_posX, cam_posY);
+	
+
+	// Direction inputs
+	if (input->isKeyDown(aie::INPUT_KEY_W))
+	{
+		while (speed < 500)
+		{
+			speed += 30;
+			if (speed > 500)
+			{
+				speed = 500;
+			}
+		}
+	}
+	else if (speed > 0)
+	{
+		speed -= 5;
+		if (speed < 0)
+		{
+			speed = 0;
+		}
+	}
+
+	if (input->isKeyDown(aie::INPUT_KEY_S) && speed > 0)
+	{
+			speed -= 20;
+			if (speed < 0)
+			{
+				speed = 0;
+			}
+	}
+	
+
+	if (input->isKeyDown(aie::INPUT_KEY_A))
+	{
+		/*playerPosX -= 100.0f * deltaTime;*/
+		rotation += 0.05;
+	}
+
+	if (input->isKeyDown(aie::INPUT_KEY_D))
+	{
+		rotation -= 0.05;
+
+		/*playerPosX += 100.0f * deltaTime;
+		m_timer += 2.0f * deltaTime;*/
+	}
+
+	/*m_2dRenderer->setCameraPos(cam_posX, cam_posY);*/
+	
+	// exit the application
+	if (input->isKeyDown(aie::INPUT_KEY_ESCAPE))
+		quit();
+
+	
+}
+
+void aieProject2D1App::draw() {
+
+	// wipe the screen to the background colour
+	clearScreen();
+
+	// begin drawing sprites
+	m_2dRenderer->begin();
+
+	// draw your stuff here!
+
+	// Background
+	m_2dRenderer->drawSprite(m_world_track, 0, 0, 1280, 720, 0, 50, 0, 0); 
+
+	// Sprite
+	m_2dRenderer->setUVRect(0, 0, 1, 1);
+	m_2dRenderer->drawSprite(m_ship, x, y, 127, 130, rotation, 1);
+	//m_2dRenderer->drawSpriteTransformed3x3(m_faceTexture, 3.0f, playerPosX, playerPosY, 36, 42, 0, 0, 0, 0);
+
+
+	/* m_2dRenderer->drawLine(100, 100, 200, 200, 5, 5); */
+	
+	// output some text, uses the last used colour
+	m_2dRenderer->drawText(m_font, "Press ESC to quit", 0, 0);
+
+	// done drawing sprites
+	m_2dRenderer->end();
+}
---
+++ aieBootstrap-master/aieProject2D1/aieProject2D1App.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include "Application.h"
+#include "Renderer2D.h"
+
+class aieProject2D1App : public aie::Application {
+public:
+
+	aieProject2D1App();
+	virtual ~aieProject2D1App();
+
+	virtual bool startup();
+	virtual void shutdown();
+
+	virtual void update(float deltaTime);
+	virtual void draw();
+
+	float playerPosX = 200;
+	float playerPosY = 200;
+
+	float x = 200.0f, y = 200.0f;
+	float rotation = 0;
+	float speed = 0;
+
+	
+	
+
+protected:
+
+	aie::Texture*		m_faceTexture;
+	aie::Texture*		m_ship;
+	aie::Renderer2D*	m_2dRenderer;
+	aie::Font*			m_font;
+	aie::Texture*		m_background;
+	aie::Texture*		m_sunny_road;
+	aie::Texture*		m_world_track;
+
+	//float max_speed = 50;
+	float m_timer;
+};
